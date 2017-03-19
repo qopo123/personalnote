633fcf47d504d09c1a32377efc257f8aab213b54
yangchuanhuahome <yangchuanhua@yonyou.com>
2017-03-19 20:30:20 +0800

    提交

--- src/com/myproject/controller/CourseController.java
+++ src/com/myproject/controller/CourseController.java
@@ -1,9 +1,22 @@
 package com.myproject.controller;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Created by Administrator on 2017/3/16.
  */
 public class CourseController {
-
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("String");
+        list.add(1,"hello");
+        Iterator<String> itr = list.iterator();
+        while(itr.hasNext()){
+            String str = itr.next();
+            System.out.println(str);
+        }
+    }
 }
 

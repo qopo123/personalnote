dbead5958573a6060ce45306519b73716d6f9a2d
yangchuanhua <yangchuanhua@yonyou.com>
2017-03-23 16:48:53 +0800

    merge测试

--- src/com/myproject/controller/CourseController.java
+++ src/com/myproject/controller/CourseController.java
@@ -8,24 +8,19 @@ import java.util.List;
  * Created by Administrator on 2017/3/16.
  */
 public class CourseController {
-<<<<<<< HEAD
   private List<String> list = new ArrayList<>();
   private List<String> list1 = new ArrayList<>();
-=======
->>>>>>> 633fcf47d504d09c1a32377efc257f8aab213b54
+
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
         list.add("String");
-        list.add(1,"hello");
+        list.add(1,"你好11");
         Iterator<String> itr = list.iterator();
         while(itr.hasNext()){
             String str = itr.next();
             System.out.println(str);
         }
+      System.out.println("hhll");
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> 633fcf47d504d09c1a32377efc257f8aab213b54
 }
 
